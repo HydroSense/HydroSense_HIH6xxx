d412f2825651aae8185e3a82d308d7b387d4f1c3
Alan Marchiori <amm042@bucknell.edu>
2015-11-24 11:04:45 -0500

    added sample program, todo: wrap this in a nice class

---
+++ checkTempHumHIH6030.ino
@@ -0,0 +1,70 @@
+
+
+// set this to the hardware serial port you wish to use
+//#define HWSERIAL Serial1
+
+#include <i2c_t3.h>
+
+
+void setup() {
+  Serial.begin( 9600 );
+  Wire.begin( I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_100 );
+}
+
+uint8_t v = 0;
+uint8_t count = 0;
+
+uint8_t target = 0x27;
+
+
+uint8_t buffer[4];
+
+float t, h;
+
+void loop() {
+
+  int i;
+  int rc;
+  
+  Wire.beginTransmission( target ); 
+  rc = Wire.endTransmission();
+  
+  if (rc != 0){
+    Serial.print("Warning, start measurement failed\n"); 
+  }
+  delay(10);
+  
+  Serial.print(count++);  
+  Serial.print(": ");
+
+  int avail = Wire.requestFrom( target, 4 );
+  
+  if (avail == 4){
+     for (i = 0; i < 4; i++){
+       buffer[i] = Wire.readByte();
+     }
+     h = ((((uint16_t) (buffer[0] & 0x3f)) << 8) | buffer[1]) / 163.820;
+     t = ((((((uint16_t) buffer[2]) << 8) | buffer[3]) >> 2) / 16382.0) * 165 - 40;
+     
+     Serial.print("temp: ");
+     Serial.print(t);
+     Serial.print(" ");
+     Serial.print("humid: ");
+     Serial.print(h);
+     
+
+       
+  }else{
+     Serial.print("request failed with ");
+     Serial.print(avail);
+
+  }
+  Serial.print("\n");  
+  
+  
+
+  
+  
+  delay(100);
+}
+
